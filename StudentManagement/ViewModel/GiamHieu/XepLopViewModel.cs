afdd0fad8713f48326ead6f06fcf1b2d2c3506eb
Dang Thai Son <sondanguit@gmail.com>
2023-04-05 11:41:45 +0700

    LayOutDanhSachXepLop

---
+++ StudentManagement/ViewModel/GiamHieu/XepLopViewModel.cs
@@ -0,0 +1,76 @@
+﻿using StudentManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StudentManagement.ViewModel.GiamHieu
+{
+    public class XepLopViewModel:BaseViewModel
+    {
+        private ObservableCollection<StudentManagement.Model.HocSinh> _danhSachHocSinh;
+        public ObservableCollection<StudentManagement.Model.HocSinh> DanhSachHocSinh { get => _danhSachHocSinh; set { _danhSachHocSinh = value; OnPropertyChanged(); } }
+        private ObservableCollection<string> _namSinhCmb;
+        public ObservableCollection<string> NamSinhCmb { get => _namSinhCmb; set { _namSinhCmb = value; OnPropertyChanged(); } }
+        public XepLopViewModel()
+        {
+            LoadNamSinh();
+            LoadDanhSachHocSinh();
+        }
+        void LoadNamSinh()
+        {
+            NamSinhCmb = new ObservableCollection<string>();
+            using (SqlConnection con = new SqlConnection(ConnectionString.connectionString))
+            {
+                con.Open();
+                string CmdString = "select distinct Year(NgaySinh) from HocSinh where TenHocSinh is not null";
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        NamSinhCmb.Add(reader.GetInt32(0).ToString());
+                    }
+                    reader.NextResult();
+                }
+                con.Close();
+            }
+        }
+        public void LoadDanhSachHocSinh()
+        {
+            DanhSachHocSinh = new ObservableCollection<Model.HocSinh>();
+            using (SqlConnection con = new SqlConnection(ConnectionString.connectionString))
+            {
+                con.Open();
+                string CmdString = "select * from HocSinh where TenHocSinh is not null";
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        StudentManagement.Model.HocSinh student = new StudentManagement.Model.HocSinh
+                        {
+                            MaHocSinh = reader.GetInt32(0),
+                            TenHocSinh = reader.GetString(1),
+                            NgaySinh = reader.GetDateTime(2),
+                            GioiTinh = reader.GetBoolean(3),
+                            DiaChi = reader.GetString(4),
+                            Email = reader.GetString(5),
+                        };
+                        DanhSachHocSinh.Add(student);
+                    }
+                    reader.NextResult();
+                }
+                con.Close();
+            }
+        }
+    }
+}
