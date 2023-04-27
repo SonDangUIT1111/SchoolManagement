1d1c196c3ee0150ef726fb325c1c176c6e78726f
Doan Le Tuan Thanh <thanh99949@gmail.com>
2023-04-27 15:51:20 +0700

    ThayDoiQuyDinh UI & Load QuyDinh to Cmb

---
+++ StudentManagement/ViewModel/GiamHieu/ThayDoiQuyDinhViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using StudentManagement.Model;
+using StudentManagement.Views.GiamHieu;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace StudentManagement.ViewModel.GiamHieu
+{
+    public class ThayDoiQuyDinhViewModel: BaseViewModel
+    {
+        private ObservableCollection<StudentManagement.Model.QuiDinh> _danhSachQuyDinh;
+        public ObservableCollection<StudentManagement.Model.QuiDinh> DanhSachQuyDinh { get => _danhSachQuyDinh; set { _danhSachQuyDinh = value; OnPropertyChanged(); } }
+        public ThayDoiQuyDinhViewModel()
+        {
+            LoadThongTinCmb();
+        }
+        public void LoadThongTinCmb()
+        {
+            DanhSachQuyDinh = new ObservableCollection<StudentManagement.Model.QuiDinh>();
+            using (SqlConnection con = new SqlConnection(ConnectionString.connectionString))
+            {
+                con.Open();
+                string CmdString = "select * from QuiDinh";
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        QuiDinh item = new QuiDinh();
+                        item.MaQuiDinh = reader.GetInt32(0);
+                        item.TenQuiDinh = reader.GetString(1);
+                        item.GiaTri = reader.GetInt32(2);
+                        DanhSachQuyDinh.Add(item);
+                    }
+                    reader.NextResult();
+                }
+                con.Close();
+            }
+        }
+    }
+    
+}
